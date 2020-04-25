fc79ead3562a06aaff1855e4190efb299a625381
DebarunMitra <debarun96mitra@gmail.com>
2020-04-25 22:44:38 +0530

    array param in useEffect

--- hook-elements-and-implementations/src/Components/UseEffect/FirstProgramWithUseEffect.js
+++ hook-elements-and-implementations/src/Components/UseEffect/FirstProgramWithUseEffect.js
@@ -4,14 +4,18 @@ function FirstProgramWithUseEffect() {
 
 
     const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
 
     useEffect(()=>{
+        console.log('useEffect: document title');
+        
          document.title = `Count clicked ${count}`
-    })
+    }, [count])//['specify state or props need to watch for the update']
 
     return (
         <div className="App">
             <p>Count: {count}</p>
+            <input type="text" value={name} onChange={e=>setName(e.target.value)} />
             <button onClick={() => setCount(count + 1)}>Count Up</button>
             <button onClick={() => setCount(count - 1)}>Count Down</button>
         </div>
