9f29daf524987a1263a47c815b03a7867761dfcd
DebarunMitra <debarun96mitra@gmail.com>
2020-04-26 17:44:17 +0530

    use reducer ex1

---
+++ hook-elements-and-implementations/src/Components/UseReducer/CounterWithReducer.js
@@ -0,0 +1,36 @@
+import React, {useReducer} from 'react';
+
+
+const initialState = 0;
+
+const reducer = (state, action) =>{
+    // return newState
+    switch(action){
+        case 'increment':
+            return state + 1
+        case 'decrement':
+            return state - 1
+        case 'reset':
+            return initialState
+        default :
+            return state
+
+    }
+}
+
+const CounterWithReducer = () => {
+
+    const [count, dispatch] = useReducer(reducer, initialState)
+
+    return (
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={() => dispatch('increment')}>Count Up</button>
+            <button onClick={() => dispatch('reset')}>Reset</button>
+            <button onClick={() => dispatch('decrement')}>Count Down</button>
+        </div>
+    );
+};
+
+
+export default CounterWithReducer;
